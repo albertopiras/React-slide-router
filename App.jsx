89dc39e986637955b0a1b3ab0b02272f081b018d
Alberto Piras <a.piras.ict@gmail.com>
2017-02-16 15:18:59 +0100

    updating routes

--- App.jsx
+++ App.jsx
@@ -6,36 +6,32 @@ var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 export class App extends React.Component {
 
     constructor(props) {
+        console.log("INIT");
         super(props);
 
         this.state = {
-            
             data: 'data 1',
-
         };
 
     };
 
     render() {
         return (
-            <div>
-
-                Page Routing
-                <br/>
-                    <div>
-                         <ReactCSSTransitionGroup
-                                component="div"
-                                transitionName="example"
-                                transitionAppear = {true} transitionAppearTimeout = {1000}
-                                transitionEnter = {true} transitionLeave = {true}>
-
-                                {React.cloneElement(this.props.children, {
-                                    key: location.href
-                                })}
-                            </ReactCSSTransitionGroup>
-                    </div>
-
-            </div>
+                <ReactCSSTransitionGroup
+                    component="div"
+                    transitionName="example"
+                    transitionAppear = {true} transitionAppearTimeout = {1000}
+                    transitionEnter = {true} transitionLeave = {true}>
+                    {React.cloneElement(this.props.children, {
+                        key: location.href
+                    })}
+                    <div id="directionButtons">
+                        <Link to="pageLeft"><button className="btn-go-to-left btn" aria-label="go to page left"></button></Link>
+                        <Link to="pageTop"><button className="btn-go-up btn" aria-label="go to page up"></button></Link>
+                        <Link to="pageDown"><button className="btn-go-down btn" aria-label="go to page down"></button></Link>
+                        <Link to="pageRight"><button className="btn-go-to-right btn" aria-label="go to page right"></button></Link>
+                 </div>
+                </ReactCSSTransitionGroup>
         );
     }
 }
@@ -44,12 +40,9 @@ export class App extends React.Component {
 export class PageCenter extends React.Component {
     render() {
         return (
-            <div>
+            <div className="page" id="pageCenter">
                 <h1>Page Center </h1>
-                <Link to="pageTop">Top </Link> <br/>
-                <Link to="pageRight">Right -&gt;</Link> <br/>
-                <Link to="pageDown">Down v</Link> <br/>
-                <Link to="pageLeft"> &lt;- Left</Link> <br/>
+                <h2>Move to right, down, left, top to route pages</h2>
             </div>
         )
     }
@@ -59,12 +52,8 @@ export class PageCenter extends React.Component {
 export class PageTop extends React.Component {
     render() {
         return (
-            <div>
+            <div className="page" id="pageTop">
                 <h1>Page TOP</h1>
-                <Link to="pageTop">Top </Link> <br/>
-                <Link to="pageRight">Right -&gt;</Link> <br/>
-                <Link to="pageDown">Down v</Link> <br/>
-                <Link to="pageLeft"> &lt;- Left</Link> <br/>
             </div>
         )
     }
@@ -74,12 +63,8 @@ export class PageTop extends React.Component {
 export class PageRight extends React.Component {
     render() {
         return (
-            <div>
+            <div className="page"  id="pageRight">
                 <h1>Page pageRight</h1>
-                <Link to="pageTop">Top </Link> <br/>
-                <Link to="pageRight">Right -&gt;</Link> <br/>
-                <Link to="pageDown">Down v</Link> <br/>
-                <Link to="pageLeft"> &lt;- Left</Link> <br/>
             </div>
         )
     }
@@ -87,12 +72,8 @@ export class PageRight extends React.Component {
 export class PageDown extends React.Component {
     render() {
         return (
-            <div>
+            <div className="page" id="pageDown">
                 <h1>Page pageDown</h1>
-                <Link to="pageTop">Top </Link> <br/>
-                <Link to="pageRight">Right -&gt;</Link> <br/>
-                <Link to="pageDown">Down v</Link> <br/>
-                <Link to="pageLeft"> &lt;- Left</Link> <br/>
             </div>
         )
     }
@@ -100,12 +81,8 @@ export class PageDown extends React.Component {
 export class PageLeft extends React.Component {
     render() {
         return (
-            <div>
+            <div className="page" id="pageLeft">
                 <h1>Page pageLeft</h1>
-                <Link to="pageTop">Top </Link> <br/>
-                <Link to="pageRight">Right -&gt;</Link> <br/>
-                <Link to="pageDown">Down v</Link> <br/>
-                <Link to="pageLeft"> &lt;- Left</Link> <br/>
             </div>
         )
     }
